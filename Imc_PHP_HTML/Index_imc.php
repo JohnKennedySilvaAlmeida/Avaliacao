250891583a34f81ca2f17f06d0876ad4824b123a
John Doe <jk_curso2018@hotmail.com>
2019-11-10 13:37:33 -0300

    10/11/2019 atualiza

---
+++ Imc_PHP_HTML/Index_imc.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+
+    <!-- Google Icon Font -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- materialize.css -->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <!-- CSS Customizado -->
+    <link rel="stylesheet" href="css/customizado.css">
+
+    <!-- viewport para mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+    <nav>
+        <div class="nav-wrapper black">
+        <a href="#" class="material-icons">fitness_center</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html"></a></li>
+            <li><a href="badges.html"></a></li>
+            <li><a href="collapsible.html"></a></li>
+        </ul>
+        </div>
+    </nav><br>
+
+    <h1 class="center blue container">IMC</h1>
+
+    <form action="Index_imc.php" method="post" class="container">       <!--   forma 2 -->
+    <!-- <form action="#" method="post" class="container">                     forma 1 -->
+
+        <label>Nome:</label>
+        <input type="text" name="nome" id="nome" placeholder="abcd" pattern="[a-z\s]+$" required/>
+          
+        <label>Altura:</label>
+        <input type="text" name="altura" id="altura" placeholder="0.00" pattern="[0-9.,]+$" required/>
+
+        <label>Peso:</label>
+        <input type="text" name="peso" id="peso" placeholder="00" pattern="[0-9.,]+$" required/>
+
+        <a class="btn-floating pulse cyan accent-2"><i class="material-icons">arrow_forward</i></a>
+
+        <input class="text-color cyan accent-2" type="submit" value="Calcular" name="enviar" id="enviar"/>
+
+    </form>
+
+    <?php
+
+      //include("funcaoIMC_1.php");        //forma 1
+      include("funcaoIMC_2.php");           // forma 2        
+
+    ?>
+
+    <br>
+    <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <!-- <h5 class="white-text">Pratique exércícios e tenha uma boa alimentação</h5> -->
+                <p class="grey-text text-lighten-4">Vida Saúde</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Copyright 
+            </div>
+          </div>
+    </footer>
+
+
+
+    <!-- JavaScript no final do body para otimizar o carregamento -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js">
+    </script>
+
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+
+</html>
---
+++ Imc_PHP_HTML/funcaoIMC_1.php
@@ -0,0 +1,57 @@
+<?php 
+
+    function imc($peso, $altura){
+
+
+        $imc = round($peso / ($altura * $altura));
+
+        if ($imc < 18.5) {
+            // return "Abaixo do peso! <br> Seu imc é {$imc}.";
+            return "<h2 class='center red container'>Abaixo do peso! <br> Seu imc é {$imc}.</h2>";
+        }
+        if ($imc >= 18.5 && $imc <= 24.5) {
+           // return " Peso normal! <br> Seu imc é {$imc}.";
+           return "<h2 class='center green container'>Peso normal! <br> Seu imc é {$imc}.</h2>";
+        }
+        if ($imc > 24.5 && $imc <= 29.9) {
+            //return "Com excesso de peso! <br> Seu imc é {$imc}.";
+            return "<h2 class='center yellow container'>Com excesso de peso! <br> Seu imc é {$imc}.</h2>";
+        }
+        if ($imc > 29.9 && $imc <= 34.9) {
+           // return "Nível de obesidade! <br> Seu imc é {$imc}.";
+           return "<h2 class='center orange container'>Nível 1 de obesidade! <br> Seu imc é {$imc}.</h2>";
+        }
+        if ($imc > 34.9 && $imc <= 39.9) {
+            //return "Nível de obesidade mórbida! <br> Seu imc é {$imc}.";
+            return "<h2 class='center red container'>Nível 2 de obesidade mórbida! <br> Seu imc é {$imc}.</h2>";
+        } else{
+            return "<h2 class='center purple darken-4 container'>Nível 3 de obesidade mórbida! <br> Seu imc é {$imc}.</h2>";
+        }
+
+    }
+
+    if (isset($_POST['enviar'])) {
+
+        $nome = $_POST['nome']; 
+        $altura = $_POST['altura'];
+        $peso = $_POST['peso'];
+
+        // str_replace(",",".", $altura);  // obs!!
+        // str_replace(",",".", $peso);   // obs!!
+
+        // $idadeNova = str_replace(",",".", $altura);
+        // echo "teste------ " . $altura;
+            
+        imc($peso, $altura);
+
+        $cauculo_imc = imc($peso, $altura);
+
+        echo "<h2 class='center'>Sr.{$nome}<i class='material-icons'>face</i></h2>";
+        echo "<h2 class='center'>{$cauculo_imc}</h2>";
+
+    }
+
+?>
+
+
+   
